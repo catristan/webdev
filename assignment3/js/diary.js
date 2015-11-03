71039706bb77960f37a94737c9fddb10ff46b29c
Claudia Tristan <003402242@coyote.csusb.edu>
2015-11-03 09:01:22 -0800

    add form

--- assignment3/js/diary.js
+++ assignment3/js/diary.js
@@ -33,7 +33,7 @@
     exports.addTxt = function () {
         $("#add-text").css("display", "block").find("input").focus();
     };
-    exports.okEdit = function () {
+    exports.addEntry = function () {
         var subject = $("#add-text input").val();
         if (!subject) {
             alert("Subject is required");
